406045d11c73cadcffc93c55c0b2d8bfc2a8da4e
tamas.nyirati <tamas.nyirati@gmail.com>
2025-06-14 15:35:15 +0200

    second commit

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string notCorrect = "How big field do you want to play? (3-9)";
+            Console.WriteLine("TicTacToe Console Game");
+            Console.Write(notCorrect);
+            string value = Console.ReadLine();
+            int result = Convert.ToInt32(value);
+            if (value == null)
+            {
+                Console.WriteLine(notCorrect);
+            }
+            else
+            {
+                 if (result < 3) { Console.WriteLine(notCorrect); }
+            }
+            
+        }
+    }
+}
--- tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44f61bf3a7bb2af9f068e1f13220a8c4a8febc2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
